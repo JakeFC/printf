62966616f6eaa5cecfe5581b722369538497d973
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-14 22:53:07 +0000

    Reup leg work

---
+++ c_ptr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *
+ * @args: input string
+ * Return: converted string
+ */
+
+char *c_ptr(va_list args)
+{
+
+  (void)args;
+  char *wang = "";
+  return (wang);
+}
