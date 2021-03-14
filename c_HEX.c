c8d25fb1d6381c40e36451600a8d43006f8c3cb6
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-14 22:45:54 +0000

    Init HEX

---
+++ c_HEX.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *
+ * @args: input string
+ * Return: converted string
+ */
+
+char *c_HEX(va_list args)
+{
+
+  (void)args;
+  char *wump = "";
+  return (wump);
+}
