281ded10356e19e06a0534b17c4fb6b65bd123f1
Christopher-Caswell <christopher-caswell@rocketmail.com>
2021-03-14 22:47:42 +0000

    Init 'S' string

---
+++ c_strung.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *
+ * @args: input string
+ * Return: converted string
+ */
+
+char *c_strung(va_list args)
+{
+
+  (void)args;
+  char *womp = "";
+  return (womp);
+}
